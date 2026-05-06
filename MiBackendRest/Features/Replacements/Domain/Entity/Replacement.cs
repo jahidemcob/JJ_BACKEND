bbdaceb7134127dc333f0204376d7a8765790a06
N0ctt <sarce@ucundinamarca.edu.co>
2026-05-05 19:01:09 -0500

    Agrego monorepo completo (Backend, Rest y Tests)

---
+++ MiBackendRest/Features/Replacements/Domain/Entity/Replacement.cs
@@ -0,0 +1,9 @@
+﻿namespace MiBackendRest.Features.Replacements.Domain.Entity
+{
+    public class Replacement
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
---
+++ MiBackendTests/Tests/Features/Motorbikes/GetByIdMotorbikes/MotorbikeNotFoundTest.cs
@@ -0,0 +1,29 @@
+﻿using Backend.src.app.Features.Motobikes.application.exceptions;
+using Backend.src.app.Features.Motobikes.application.usecases;
+using Backend.src.app.Features.Motobikes.domain.repository;
+using Moq;
+using Xunit;
+
+
+namespace MiBackendTests.Tests.Features.Motorbikes.GetByIdMotorbikes;
+public class MotorbikeNotFoundTest
+{
+    [Fact]
+    public async Task GetMotorbikeById_ShouldThrowNotFoundException_WhenMotorbikeDoesNotExist()
+    {
+        // Arrange
+        var repoMock = new Mock<IMotorbikesRepository>();
+        int motorbikeId = 10;
+
+        // Configuramos el repositorio para que devuelva null (como si no existiera la moto)
+        repoMock.Setup(r => r.GetMotorbikeByIdAsync(motorbikeId))
+                .ReturnsAsync((Backend.src.app.Features.Motobikes.domain.entities.Motorbike?)null);
+
+        var useCase = new GetByIdMotorbikeUsecase(repoMock.Object);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<MotorbikeNotFoundException>(() =>
+            useCase.Execute(motorbikeId)
+        );
+    }
+}
