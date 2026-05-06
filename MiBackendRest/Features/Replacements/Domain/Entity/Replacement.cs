4be4be179b473c1275e6b3fe168ad2c39838511b
N0ctt <sarce@ucundinamarca.edu.co>
2026-05-05 19:29:13 -0500

    Revert "Agrego monorepo completo (Backend, Rest y Tests)"
    This reverts commit bbdaceb7134127dc333f0204376d7a8765790a06.

--- MiBackendRest/Features/Replacements/Domain/Entity/Replacement.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MiBackendRest.Features.Replacements.Domain.Entity
-{
-    public class Replacement
-    {
-        public int Id { get; set; }
-        public required string Nombre { get; set; }
-        public decimal Precio { get; set; }
-    }
-}
